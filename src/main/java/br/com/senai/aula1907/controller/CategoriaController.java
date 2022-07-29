c8626f678c17b5e4659cfb50f095adebe9a8947f
Tiago Caceraghi dos Santos <tiago.caceraghi@edu.sc.senai.br>
2022-07-29 19:33:39 -0300

    Versao 0.0.5 - Projeto UC 8

--- src/main/java/br/com/senai/aula1907/controller/CategoriaController.java
+++ src/main/java/br/com/senai/aula1907/controller/CategoriaController.java
@@ -1,11 +1,18 @@
 package br.com.senai.aula1907.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.com.senai.aula1907.servico.CategoriaServico;
+
 @Controller
 public class CategoriaController {
+	
+	@Autowired
+	private CategoriaServico servico;
+	
 	@GetMapping({"/grupo", "/"})
 	public String listarGrupo(Model modelo) {
 		modelo.addAttribute("grupo", servico.listarCategorias());
---
+++ src/main/java/br/com/senai/aula1907/servico/CategoriaServicoCRUD.java
@@ -0,0 +1,19 @@
+package br.com.senai.aula1907.servico;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.senai.aula1907.orm.Categoria;
+import br.com.senai.aula1907.repository.CategoriaRepository;
+
+@Service
+public class CategoriaServicoCRUD implements CategoriaServico{
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public List<Categoria> listarCategorias(){
+		return (List<Categoria>) repository.findAll();
+	}
+}
